5e7a49cbe03b4457d45ffac08a1a7e024f7eb6e7
Tiago Ribeiro <tiago.err@outlook.com>
2022-09-05 23:57:26 +0100

    Revamped the home page to use a new design with just cards, making it a bit faster and easier to use

--- components/RestaurantCard/index.tsx
+++ components/RestaurantCard/index.tsx
@@ -1,55 +1,38 @@
 import {Restaurant} from "../../interfaces";
-import QRCode from "react-qr-code";
-import {useEffect, useState} from "react";
 import {AnimatePresence, motion} from "framer-motion";
 
 export interface Props extends Restaurant {
 	userAmount: number;
-	qrCode: string;
 	onClick?: () => void;
 }
 
 export default function RestaurantCard(props: Props) {
-	const [showQRCode, setShowQRCode] = useState(false);
-	const [hasBeenClicked, setHasBeenClicked] = useState(false);
-
-	useEffect(() => {
-		if (showQRCode) setHasBeenClicked(true);
-	}, [showQRCode]);
-
 	const computeIndicatorColor = () => {
-		return props.rewardAmount - props.userAmount === 0 ? "bg-success border-success" : "bg-primary border-primary";
+		switch (props.rewardAmount - props.userAmount) {
+			case 0:
+				return "bg-success border-success";
+			case 1:
+				return "bg-amber-500 border-amber-500";
+			default:
+				return "bg-info border-info";
+		}
 	};
 
 	return (
 		<AnimatePresence>
 			<motion.div className="indicator text-center" whileHover={{scale: 1.1}} whileTap={{scale: 0.8}} onClick={props.onClick}>
-				{!showQRCode && (
-					<div className="flex flex-col w-full">
-						<span
-							className={`indicator-item badge text-white font-semibold text-lg rounded-xl w-9 h-9 mr-2 mt-2 ${computeIndicatorColor()}`}>
-							{props.rewardAmount - props.userAmount === 0 ? "!" : props.rewardAmount - props.userAmount}
-						</span>
-						<motion.div
-							exit={{scale: 0, opacity: 0}}
-							initial={{scale: 0.3, opacity: 0}}
-							animate={{scale: 1, opacity: 1}}
-							className="rounded-xl p-8 flex items-center justify-center w-48 h-32 drop-shadow-md"
-							style={{color: props.color, backgroundColor: props.bgColor}}>
-							<h2 className="font-bold text-xl">{props.name}</h2>
-						</motion.div>
-						{!hasBeenClicked && <span className="font-light italic text-sm">Click to show the QR Code</span>}
-					</div>
-				)}
-				{showQRCode && (
-					<motion.div
-						exit={{scale: 0, opacity: 0}}
-						initial={{scale: 0.3, opacity: 0}}
-						animate={{scale: 1, opacity: 1}}
-						className="h-32 w-48 flex items-center justify-center">
-						<QRCode value={props.qrCode} size={128} title={props.name} level="H" />
-					</motion.div>
-				)}
+				<span
+					className={`indicator-item badge text-white font-semibold text-lg rounded-xl w-9 h-9 mr-2 mt-2 drop-shadow-[0_0_0.75rem_rgba(0,0,0,0.25)] ${computeIndicatorColor()}`}>
+					{props.rewardAmount - props.userAmount === 0 ? "!" : props.rewardAmount - props.userAmount}
+				</span>
+				<motion.div
+					exit={{scale: 0, opacity: 0}}
+					initial={{scale: 0.3, opacity: 0}}
+					animate={{scale: 1, opacity: 1}}
+					className="rounded-xl p-8 flex items-center justify-center w-44 h-28 drop-shadow-xl"
+					style={{color: props.color, backgroundColor: props.bgColor}}>
+					<h2 className="font-bold text-xl drop-shadow-2xl">{props.name}</h2>
+				</motion.div>
 			</motion.div>
 		</AnimatePresence>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,22 @@
-import {collection, getDocs} from "firebase/firestore";
 import type {NextPage} from "next";
 import Head from "next/head";
-import Image from "next/image";
 import Router from "next/router";
-import {useContext, useEffect, useState} from "react";
+import {Fragment, useContext, useEffect, useState} from "react";
 import Header from "../components/Header";
-import RestaurantCard, {Props} from "../components/RestaurantCard";
-import RestaurantCarousel from "../components/RestaurantCarousel";
-import RewardCounter from "../components/RewardCounter";
+import RestaurantCard from "../components/RestaurantCard";
 import {AuthContext} from "../firebase/AuthContext";
 import useRestaurants from "../hooks/useRestaurants";
 import useUserRestaurants from "../hooks/useUserRestaurants";
 import {CompleteRestaurant, Restaurant} from "../interfaces";
+import {Transition} from "@headlessui/react";
+import QrCodeModal from "../components/QrCodeModal";
 
 const Home: NextPage = () => {
 	const {user} = useContext(AuthContext);
 	const {restaurants, isLoading: isLoadingRestaurants} = useRestaurants();
 	const {userRestaurants, isLoading: isUserRestaurantsLoading} = useUserRestaurants(user);
 	const [selectedRestaurant, selectRestaurant] = useState<CompleteRestaurant | undefined>(undefined);
+	const [qrModalOpen, setQrModalOpen] = useState(false);
 
 	useEffect(() => {
 		if (!user) {
@@ -26,8 +25,8 @@ const Home: NextPage = () => {
 	}, [user]);
 
 	useEffect(() => {
-		console.log({userRestaurants});
-	}, [userRestaurants]);
+		if (!qrModalOpen) setTimeout(() => selectRestaurant(undefined), 300);
+	}, [qrModalOpen]);
 
 	const completeRestaurant = (restaurant: Restaurant): CompleteRestaurant => {
 		const userRestaurant = getUserRestaurantFromRestaurantID(restaurant.id);
@@ -47,16 +46,24 @@ const Home: NextPage = () => {
 			</Head>
 
 			<main className="h-screen w-full lg:w-5/12 flex flex-col items-center justify-center bg-white dark:bg-gray-800 lg:rounded-xl drop-shadow-xl">
-				<div className="p-8 w-full mb-8">
+				<div className="p-8 w-full mb-4">
 					<Header user={user} />
 				</div>
-				<div className="mt-8 h-full w-full grid grid-cols-1 lg:grid-cols-2 gap-8 place-content-start place-items-center ">
+				<div className="mt-4 h-full w-full grid grid-cols-2 place-content-start place-items-center px-1">
 					{restaurants &&
 						userRestaurants &&
-						restaurants
-							.map(completeRestaurant)
-							.map((restaurant) => <RestaurantCard key={restaurant.id} {...restaurant} onClick={() => selectRestaurant(restaurant)} />)}
+						restaurants.map(completeRestaurant).map((restaurant) => (
+							<RestaurantCard
+								key={restaurant.id}
+								{...restaurant}
+								onClick={() => {
+									selectRestaurant(restaurant);
+									setQrModalOpen(true);
+								}}
+							/>
+						))}
 				</div>
+				<QrCodeModal isOpen={qrModalOpen} onClose={() => setQrModalOpen(false)} qrCode={selectedRestaurant?.qrCode || "qrCode"} />
 			</main>
 		</div>
 	);
